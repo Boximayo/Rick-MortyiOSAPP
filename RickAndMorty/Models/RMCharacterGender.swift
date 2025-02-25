6a55b49680002ee1932fed93fda7d7eeb4e9d82b
mayo.g <mayo_black@outlook.com>
2025-02-25 01:44:14 -0700

    Add Models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Mayo Gonzalez ortega on 25/02/25.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Female"
+    case genderless = "Genderless"
+    case ´unknown´ = "unknown"
+}
--- RickAndMorty/Models/RmEpisode.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RmEpisode.swift
-//  RickAndMorty
-//
-//  Created by Mayo Gonzalez ortega on 22/02/25.
-//
-
-import Foundation
